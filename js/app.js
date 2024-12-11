f5d4a5cacae840845cdcb14b4a9b96e78cb2ed7f
AlissonSimon <alissonsimon21@gmail.com>
2024-12-11 18:14:32 -0300

    Comprar ingresso Cadeiras

--- js/app.js
+++ js/app.js
@@ -4,20 +4,53 @@ function comprar() {
     // Transforma de string para número inteiro e recebe somente o valor
     let qtd = parseInt(document.getElementById('qtd').value);
 
-    if(tipo.value == 'pista') {
+    if (tipo.value == 'pista') {
         comprarPista(qtd);
+    }
+
+    if (tipo.value == 'inferior') {
+        comprarInferior(qtd);
+    }
+
+    if (tipo.value == 'superior') {
+        comprarSuperior(qtd);
     }
 }
 
 function comprarPista(qtd) {
     // Transforma a variável em número inteiro e relaciona ao HTML
     let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
-    if(qtd > qtdPista) {
-        alert('Esta quantidade está indisponível para ingressos de tipo pista.');
+
+    if (qtd > qtdPista) {
+        alert ('Essa quantidade está indisponível para ingressos do tipo pista.');
     } else {
         qtdPista = qtdPista - qtd;
         // Atualiza o valor no HTML através da subtração
         document.getElementById('qtd-pista').textContent = qtdPista;
-        alert('Compra realizada com sucesso!');
+        alert ('Compra realizada com sucesso!');
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+
+    if (qtd > qtdInferior) {
+        alert ('Essa quantidade está indisponível para ingressos do tipo cadeira inferior.')
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert ('Compra realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+
+    if (qtd > qtdSuperior) {
+        alert ('Essa quantidade está indisponível para ingressos do tipo cadeira superior.')
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert ('Compra realizada com sucesso!');
     }
 }
